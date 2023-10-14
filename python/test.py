d2175e3bce6cc0f22303594845abf9b9d26380ab
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-10-14 17:42:55 -0400

    aim to  all scripts

--- python/test.py
+++ python/test.py
@@ -44,9 +44,9 @@ def analyze_script_coverage(script_code, external_code_map):
 
             if function_name:
                 start_line, end_line = None, None
-                if package and function_name in external_code_map:
+                if package and package in external_code_map:
                     start_line, end_line = analyze_function(
-                        external_code_map[function_name], function_name.split('.')[-1])
+                        external_code_map[package], function_name.split('.')[-1])
 
                 analysis_result['external_calls'].append({
                     'function': function_name,
@@ -55,12 +55,16 @@ def analyze_script_coverage(script_code, external_code_map):
                     'lines_in_package': f"{start_line} to {end_line}" if start_line and end_line else None
                 })
 
-    with open('analysis_result.json', 'w') as f:
+    with open('./python/analysis_result.json', 'w') as f:
         json.dump(analysis_result, f, indent=4)
 
+# returns the path to the module if it exists, None otherwise
 def get_module_path(module_name):
     try:
         module = importlib.import_module(module_name)
+        if not hasattr(module, '__file__'):
+            print(f"{module_name} is a built-in module, skipping.")
+            return None
         module_path = inspect.getfile(module)
         return module_path
     except Exception as e:
@@ -76,11 +80,24 @@ if __name__ == "__main__":
     with open(args.file_path, 'r') as f:
         script_code = f.read()
 
+    # analyze the script to get the list of imported modules
+    import_map = {}
+    tree = ast.parse(script_code)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for n in node.names:
+                import_map[n.name.split('.')[0]] = n.name
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module
+            for n in node.names:
+                import_map[n.name] = f"{module}.{n.name}"
+
+    # populate external_code_map based on the imported modules
     external_code_map = {}
-    module_name = 'img2pdf'
-    module_path = get_module_path(module_name)
-    if module_path:
-        with open(module_path, 'r') as f:
-            external_code_map[f"{module_name}.convert"] = f.read()
+    for module_name, full_module_name in import_map.items():
+        module_path = get_module_path(module_name)
+        if module_path:
+            with open(module_path, 'r') as f:
+                external_code_map[full_module_name] = f.read()
 
     analyze_script_coverage(script_code, external_code_map)
