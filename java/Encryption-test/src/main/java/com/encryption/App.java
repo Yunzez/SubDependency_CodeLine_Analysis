71023cdf39adc5ba06e976be21c2330fac5881b2
Yuchen Zhang <yzhang0701@gmail.com>
2023-10-31 12:45:07 -0400

    Add: adding jasypt encryption

--- java/Encryption-test/src/main/java/com/encryption/App.java
+++ java/Encryption-test/src/main/java/com/encryption/App.java
@@ -1,11 +1,11 @@
 package com.encryption;
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.util.text.BasicTextEncryptor;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Security;
 import java.util.Base64;
-
-
 
 public class App {
     static {
@@ -16,19 +16,32 @@ public class App {
         String text = "America runs on dunkin!";
         String key = "1234567812345678"; // 16 chars=128 bits
 
+        // Using BouncyCastle
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
         SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
-
         System.out.println("original text: " + text);
-        // Encryption
+
+        // Encryption with BouncyCastle
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         byte[] encrypted = cipher.doFinal(text.getBytes());
         String encryptedBase64 = Base64.getEncoder().encodeToString(encrypted);
-        System.out.println("Encrypted: " + encryptedBase64);
+        System.out.println("BouncyCastle Encrypted: " + encryptedBase64);
 
-        // Decryption
+        // Decryption with BouncyCastle
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
         byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedBase64));
-        System.out.println("Decrypted: " + new String(decrypted));
+        System.out.println("BouncyCastle Decrypted: " + new String(decrypted));
+
+        // Using Jasypt
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword(key);
+
+        // Encryption with Jasypt
+        String encryptedText = textEncryptor.encrypt(text);
+        System.out.println("Jasypt Encrypted: " + encryptedText);
+
+        // Decryption with Jasypt
+        String decryptedText = textEncryptor.decrypt(encryptedText);
+        System.out.println("Jasypt Decrypted: " + decryptedText);
     }
 }
